4d97b5048f6977eef906867df70611569d3bb44b
Savannah <12411476+savannah-yahsuanlin@users.noreply.github.com>
2022-10-26 12:10:42 +0800

    Update Assets.js

--- src/Assets.js
+++ src/Assets.js
@@ -7,21 +7,21 @@ const Assets = () => {
 
 	return (
 		<div className="cards">
-			{
-				assets.map(asset => {
-					return (
-						<div className="card" key={asset.id}>
-							<Link to={`/assets/${asset.id}`}>
-								<div>
-									<span>{asset.id}</span>
-									<h2>{asset.collectionName }</h2>
-									<img src={asset.image_url} alt={asset.name} />
-								</div>
-							</Link>
-					</div>
-					)
-				})
-			}
+		{
+		assets.map(asset => {
+		return (
+			<div className="card" key={asset.id}>
+			<Link to={`/assets/${asset.id}`}>
+			<div>
+			<span>{asset.id}</span>
+			<h2>{asset.collectionName }</h2>
+			<img src={asset.image_url} alt={asset.name} />
+			</div>
+			</Link>
+			</div>
+		)
+		})
+		}
 		</div>
 	)
 }
