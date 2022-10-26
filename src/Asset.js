77e51daa7c25f779a5cb1520598dabc47ea293c3
Savannah <12411476+savannah-yahsuanlin@users.noreply.github.com>
2022-10-26 12:09:33 +0800

    Update Asset.js

--- src/Asset.js
+++ src/Asset.js
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom';
 
 const Asset = () => {
   const { id } = useParams();
-	const { assets } = useSelector(state => state)
-	const asset = assets.find(asset => asset.id === id*1)
+  const { assets } = useSelector(state => state)
+  const asset = assets.find(asset => asset.id === id*1)
   return (
-		<div>
-			<h1> <span>></span> <a href={`${asset.permalink}`} target="_blank">{asset.collectionName}</a></h1>
-			<img src={asset.image_url}/>
-			<h2>Name: {asset.name ? asset.name : 'Null'}</h2>
-			<h2>Description: {asset.description}</h2>
-			<h4><Link to='/assets'>Go Back</Link></h4>
-		</div>
-	);
+	<div>
+		<h1> <span>></span> <a href={`${asset.permalink}`} target="_blank">{asset.collectionName}</a></h1>
+		<img src={asset.image_url}/>
+		<h2>Name: {asset.name ? asset.name : 'Null'}</h2>
+		<h2>Description: {asset.description}</h2>
+		<h4><Link to='/assets'>Go Back</Link></h4>
+	</div>
+   );
 };
 
 export default Asset;
